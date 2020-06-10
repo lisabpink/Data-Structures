fb52d913559f14e9c60969038a93c5e9e3184a0e
LISA_COOK <lisabpink@gmail.com>
2020-06-10 18:26:07 -0400

    binary_search_tree.py for_each, edit get_max

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -42,36 +42,46 @@ class BSTNode:
 
     # Return the maximum value found in the tree
     def get_max(self):
-        return self.right.get_max() if self.right else self.value
+        if self.right is None:
+            return self.value
+        else:
+            return self.right.get_max()
 
-#     # Call the function `fn` on the value of each node
-#     def for_each(self, fn):
-#         pass
+    # Call the function `fn` on the value of each node
+    def for_each(self, fn):
+        self.value = fn(self.value)
+        if self.left:
+            self.left.for_each(fn)
+        if self.right:
+            self.right.for_each(fn)
+
 
 #    # Part 2 -----------------------
 
-#    # Print all the values in order from low to high
-#    # Hint:  Use a recursive, depth first traversal
-#    def in_order_print(self, node):
-#         pass
+   # Print all the values in order from low to high
+   # Hint:  Use a recursive, depth first traversal
 
-#     # Print the value of every node, starting with the given node,
-#     # in an iterative breadth first traversal
-#     def bft_print(self, node):
-#         pass
+        def in_order_print(self, node):
+            pass
 
-#     # Print the value of every node, starting with the given node,
-#     # in an iterative depth first traversal
-#     def dft_print(self, node):
-#         pass
+    # Print the value of every node, starting with the given node,
+    # in an iterative breadth first traversal
 
-#     # Stretch Goals -------------------------
-#     # Note: Research may be required
+    def bft_print(self, node):
+        pass
 
-#     # Print Pre-order recursive DFT
-#     def pre_order_dft(self, node):
-#         pass
+    # Print the value of every node, starting with the given node,
+    # in an iterative depth first traversal
+    def dft_print(self, node):
+        pass
 
-#     # Print Post-order recursive DFT
-#     def post_order_dft(self, node):
-#         pass
+    # Stretch Goals -------------------------
+    # Note: Research may be required
+
+    # Print Pre-order recursive DFT
+    def pre_order_dft(self, node):
+        pass
+
+    # Print Post-order recursive DFT
+    def post_order_dft(self, node):
+        pass
