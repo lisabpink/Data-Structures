93fdfad8df77ce2b51868cf2407c3be002bda0ec
LISA_COOK <lisabpink@gmail.com>
2020-06-10 14:56:38 -0400

    delete and get_max

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -103,7 +103,19 @@ class DoublyLinkedList:
     Returns the value of the removed Node."""
 
     def remove_from_tail(self):
-        pass
+        if not self.tail:
+            if not self.head:
+                return None
+        else:
+            current = self.head
+            previous = None
+            while current.next is not None:
+                previous = current
+                current = current.next
+            previous = self.tail
+            previous.next = None
+            self.length -= 1
+            return current
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
@@ -121,12 +133,35 @@ class DoublyLinkedList:
     the node was the head or the tail"""
 
     def delete(self, node):
-        pass
+        # decrement length of DLL
+        self.length -= 1
+
+        # if DLL is empty, there is nothing to delete, we should return
+        if not self.head and not self.tail:
+            return
+
+        # if DLL has one element, remove it by
+        # setting head and tail pointers to None
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        elif self.head == node:
+            self.head = node.next
+            node.delete()
+        elif self.tail == node:
+            self.tail = node.prev
+            node.delete()
 
     """Returns the highest value currently in the list"""
 
     def get_max(self):
-        pass
+        if not self.head:
+            return None
+        max_val = self.head.value
+        current = self.head
+        while current:
+            if current.value > max_val:
+                max_val = current.value
 
 
 dll = DoublyLinkedList()
